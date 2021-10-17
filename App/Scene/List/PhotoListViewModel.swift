5580a01ec1c3b1c22fb198cf90a2c882163ff6da
Rasel <r-miah@winas.jp>
2021-10-17 23:07:07 +0900

    [Update] split binding code in individual functions

--- App/Scene/List/PhotoListViewModel.swift
+++ App/Scene/List/PhotoListViewModel.swift
@@ -43,8 +43,30 @@ class PhotoListViewModel: ViewModelType {
     }
     
     func bind() {
-        
-        let searchWithText = _searchText
+        // preapare search query
+        let searchQuery = prepareSearchQuery()
+        // fetch photos with the query
+        prepareSearchResult(with: searchQuery)
+        // load photos of next page (if any)
+        prepareNextPageResult()
+        // observe selected item
+        observeSelectedItem()
+        // Input & Output
+        input = Input(
+            searchText: _searchText.asObserver(),
+            itemSelected: _itemSelected.asObserver(),
+            loadMore: _loadMore.asObserver()
+        )
+        output = Output(
+            photos: _photos.asObservable(),
+            detail: _detail.asObservable()
+        )
+    }
+    
+    /// Prepare `Observable` string with the text of search bar
+    /// - Returns: `Observable`
+    private func prepareSearchQuery() -> Observable<String> {
+        let searchQuery = _searchText
             .debounce(.milliseconds(500), scheduler: ConcurrentMainScheduler.instance)
             .do(onNext: { [weak self] text in
                 guard let self = self else { return }
@@ -63,16 +85,25 @@ class PhotoListViewModel: ViewModelType {
                 }
             }
             .share()
+        return searchQuery
+    }
     
-        // Fetch Photos
-        let searchResult = searchWithText
+    /// Prepare search result with search query
+    /// - Parameter query: `Observable<String>`
+    private func prepareSearchResult(with query: Observable<String>) {
+        let searchResult = query
             .flatMap { [weak self] text -> Observable<Event<[Photo]>> in
                 guard let self = self else { return .empty() }
                 let page = self.currentPage > 1 ? self.currentPage : 1
                 return self.fetchFeed(query: text, page: page)
                     .do(onNext: {
-                        self.currentPage = $0.page
-                        self.currentQuery = text
+                        if $0.nextPage == nil {
+                            self._loadMore.onCompleted()
+                        } else {
+                            // set current page & search query
+                            self.currentPage = $0.page
+                            self.currentQuery = text
+                        }
                     })
                     .map { feed -> [Photo] in
                         if self.isPaginated {
@@ -84,13 +115,15 @@ class PhotoListViewModel: ViewModelType {
                     .materialize()
             }
             .share()
-            
-            searchResult
-                .flatMap { $0.element.map(Observable.just) ?? .empty() }
-                .bind(to: _photos)
-                .disposed(by: disposeBag)
         
-        // Load photos from next page
+        searchResult
+            .flatMap { $0.element.map(Observable.just) ?? .empty() }
+            .bind(to: _photos)
+            .disposed(by: disposeBag)
+    }
+    
+    /// Fetch the data of next page
+    private func prepareNextPageResult() {
         _loadMore
             .bind { [weak self] in
                 guard let self = self else { return }
@@ -98,27 +131,22 @@ class PhotoListViewModel: ViewModelType {
                 self._searchText.onNext(self.currentQuery)
             }
             .disposed(by: disposeBag)
-        
-        // Item Selected
+    }
+    
+    /// Get Selected Photo Information
+    private func observeSelectedItem() {
         _itemSelected
-            .map { indexPath ->  Photo in
+            .map { [unowned self] indexPath ->  Photo in
                 return self._photos.value[indexPath.row]
             }
             .bind(to: _detail)
             .disposed(by: disposeBag)
-        
-        // Input & Output
-        input = Input(
-            searchText: _searchText.asObserver(),
-            itemSelected: _itemSelected.asObserver(),
-            loadMore: _loadMore.asObserver()
-        )
-        output = Output(
-            photos: _photos.asObservable(),
-            detail: _detail.asObservable()
-        )
     }
     
+    /// Request for `PhotoFeed`
+    /// - Parameter query: Any String.
+    /// - Parameter page: Desired page
+    /// - Returns: `Observable<PhotoFeed>`
     private func fetchFeed(query: String, page: Int) -> Observable<PhotoFeed> {
         let request = PhotoFeedRequest(searchTerm: query, page: page)
         let task = APIClient.shared.send(request)
